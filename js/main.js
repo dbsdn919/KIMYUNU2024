37822e687968f677fd77c12226e640733cb319d4
dbsdn919 <130467287+dbsdn919@users.noreply.github.com>
2023-08-30 02:36:59 +0900

    Update main.js
    Change reveal date

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /*Countdown*/
-var countDownDate = new Date("Sep 3, 2023 18:00:00").getTime();
+var countDownDate = new Date("Sep 18, 2023 18:00:00").getTime();
 var x = setInterval(function(){
     var now = new Date().getTime();
     var distance = countDownDate - now;
